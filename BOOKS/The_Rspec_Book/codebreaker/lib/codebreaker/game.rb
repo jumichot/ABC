d65e7db1e5955c23be6d74b2e233c29538fb02d5
Julien Michot <julien@captive.fr>
2013-12-04 21:08:35 +0100

    Met la class marker dans un fichier

--- BOOKS/The_Rspec_Book/codebreaker/lib/codebreaker/game.rb
+++ BOOKS/The_Rspec_Book/codebreaker/lib/codebreaker/game.rb
@@ -14,33 +14,5 @@ module Codebreaker
       @marker = Marker.new(@secret, guess)
       @output.puts('+' * @marker.exact_match_count + '-' * @marker.number_match_count)
     end
-
-    class Marker
-      def initialize secret, guess
-        @guess = guess
-        @secret = secret
-      end
-
-      def number_match_count
-        (0..3).inject(0) do |count,index|
-          count + (number_match?(index)? 1 : 0)
-        end
-      end
-
-      def exact_match_count
-        (0..3).inject(0) do |count,index|
-          count + (exact_match?(index)? 1 : 0)
-        end
-      end
-
-      def number_match? index
-        @secret.include?(@guess[index]) && !exact_match?(index)
-      end
-
-      def exact_match? index
-        @secret[index] == @guess[index]
-      end
-    end
-
   end
 end
