f13453c4c70e97502c7a944bc6b1c937dae250d2
Julien Michot <julien@captive.fr>
2013-12-10 23:05:42 +0100

    Fizzbuzz renvoit 4 bonnes réponses

--- CHALLENGES/FizzBuzz/spec/fizzbuzz_spec.rb
+++ CHALLENGES/FizzBuzz/spec/fizzbuzz_spec.rb
@@ -1,19 +1,32 @@
 module FizzBuzz
   class Reporter
     def to_a limit
-      (1..limit).to_a
+      ary = []
+      (1..limit).each_with_index do |num, index|
+        if (index+1)%3 == 0
+          ary << "Fizz"
+        else
+          ary << num
+        end
+      end
+      ary
     end
   end
 end
 
 module FizzBuzz
   describe Reporter do
-    it "reporte correctly the first line" do
+    it "reports correctly the first line" do
       expect(subject.to_a(1)).to eq [1]
     end
-
-    it "reporte correctly the first line" do
+    it "reports correctly the first 2 lines" do
       expect(subject.to_a(2)).to eq [1,2]
+    end
+    it "reports correctly the first 3 lines" do
+      expect(subject.to_a(3)).to eq [1,2,"Fizz"]
+    end
+    it "reports correctly the first 4 lines" do
+      expect(subject.to_a(4)).to eq [1,2,"Fizz",4]
     end
   end
 end
