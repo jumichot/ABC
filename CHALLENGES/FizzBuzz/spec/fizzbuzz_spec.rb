c0696dc3127b3922a303036accb4089410476f68
Julien Michot <julien@captive.fr>
2013-12-10 21:51:22 +0100

    Début le kata en tdd

--- CHALLENGES/FizzBuzz/spec/fizzbuzz_spec.rb
+++ CHALLENGES/FizzBuzz/spec/fizzbuzz_spec.rb
@@ -1,10 +1,19 @@
-require 'spec_helper'
 module FizzBuzz
   class Reporter
+    def to_a limit
+      (1..limit).to_a
+    end
   end
 end
+
 module FizzBuzz
   describe Reporter do
+    it "reporte correctly the first line" do
+      expect(subject.to_a(1)).to eq [1]
+    end
 
+    it "reporte correctly the first line" do
+      expect(subject.to_a(2)).to eq [1,2]
+    end
   end
 end
