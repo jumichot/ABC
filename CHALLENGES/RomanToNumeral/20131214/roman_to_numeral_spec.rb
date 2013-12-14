5461e1f6b1b406169b49ac873226058dd0b7a6d7
Julien Michot <julien@captive.fr>
2013-12-14 22:19:02 +0100

    Converti jusqu'a 8

--- CHALLENGES/RomanToNumeral/20131214/roman_to_numeral_spec.rb
+++ CHALLENGES/RomanToNumeral/20131214/roman_to_numeral_spec.rb
@@ -1,8 +1,28 @@
 class RomanToNumeral
+  def convert num
+    return "V"+convert(num-5) if num >= 5
+    return "IV" if num == 4
+    "I"*num
+  end
 end
 
 describe RomanToNumeral do
-  it "#convert" do
+  it "#convert 1" do
     expect(subject.convert(1)).to eq "I"
+  end
+  it "#convert 2" do
+    expect(subject.convert(2)).to eq "II"
+  end
+  it "#convert 4" do
+    expect(subject.convert(4)).to eq "IV"
+  end
+  it "#convert 5" do
+    expect(subject.convert(5)).to eq "V"
+  end
+  it "#convert 6" do
+    expect(subject.convert(6)).to eq "VI"
+  end
+  it "#convert 8" do
+    expect(subject.convert(8)).to eq "VIII"
   end
 end
