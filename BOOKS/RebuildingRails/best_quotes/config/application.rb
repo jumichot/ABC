6dd163522e03092a2a709baa6798d6230bd66586
julien michot <ju.michot@gmail.com>
2015-02-14 06:52:09 +0100

    Add first controller

--- BOOKS/RebuildingRails/best_quotes/config/application.rb
+++ BOOKS/RebuildingRails/best_quotes/config/application.rb
@@ -1,3 +1,6 @@
+$LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
+require "quotes_controller"
+
 require 'rulers'
 
 module BestQuotes
--- BOOKS/RebuildingRails/rulers/lib/rulers.rb
+++ BOOKS/RebuildingRails/rulers/lib/rulers.rb
@@ -4,8 +4,19 @@ require "rulers/array"
 module Rulers
   class Application
     def call(env)
-      `echo debug > debug.txt`
-      [200, {"Content-Type" => "text/html"},["hello from ruler"]]
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
+      [200, {"Content-Type" => "text/html"},[text]]
+    end
+  end
+
+  class Controller
+    def initialiaze(env)
+      @env = env
+    end
+    def env
+      @env
     end
   end
 end
