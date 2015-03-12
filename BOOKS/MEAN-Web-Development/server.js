4397c2f88e3d58bce1bc3dafd923b2c82d8e333e
julien michot <ju.michot@gmail.com>
2015-03-12 18:22:05 +0100

    Update the files for the book on mean stack

---
+++ BOOKS/MEAN-Web-Development/server.js
@@ -0,0 +1,18 @@
+var connect = require('connect');
+var app = connect();
+
+var logger = function(req, res, next){
+  console.log(req.method, req.url);
+  next();
+}
+
+var helloWorld = function(req, res, next){
+  res.setHeader('Content-Type','text/plain');
+  res.end('Hello World');
+}
+
+app.use(logger);
+app.use(helloWorld);
+app.listen(3000);
+
+console.log('serveur is running on port 3000');
