2a1f5e32d2373f08c71c4e495fe388c6cd81b909
julien michot <ju.michot@gmail.com>
2015-03-13 15:30:56 +0100

    Add some modules and environement variables

--- BOOKS/MEAN-Web-Development/server.js
+++ BOOKS/MEAN-Web-Development/server.js
@@ -1,13 +1,9 @@
-var express = require('express');
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var express = require('./config/express.js');
+
 var app = express();
-
-app.use('/', function(req,res){
-  console.log(req.query);
-  console.log(req.params);
-  res.send('Hello World');
-});
-
 app.listen(3000);
-console.log('Server running on 3000');
-
 module.exports = app;
+
+console.log("Server running at http://localhost:3000/");
