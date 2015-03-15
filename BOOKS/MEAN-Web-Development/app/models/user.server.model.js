3803b18ed7d28460cceda68733ed7123709a2ed6
julien michot <ju.michot@gmail.com>
2015-03-15 14:45:17 +0100

    Setup passport for the user model: add salted password, unique username

--- BOOKS/MEAN-Web-Development/app/models/user.server.model.js
+++ BOOKS/MEAN-Web-Development/app/models/user.server.model.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose'),
+  crypto = require('crypto'),
   Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
@@ -7,13 +8,13 @@ var UserSchema = new Schema({
   email: {
     type: String,
     index: true,
-    match: /.+\@.+\..+/
+    match: [/.+\@.+\..+/, "please fill a valid email address"]
   },
   username: {
     type: String,
     trim: true,
     unique: true,
-    required: true
+    required: 'Username is required'
   },
   password:{
     type: String,
@@ -24,6 +25,15 @@ var UserSchema = new Schema({
       'Password should be longer'
     ]
   },
+  salt: {
+    type: String
+  },
+  provider: {
+    type: String,
+    require: 'Provider is required'
+  },
+  providerId: String,
+  providerDate: {},
   created: {
     type: Date,
     default: Date.now()
@@ -60,17 +70,48 @@ UserSchema.statics.findOneByUsername = function(username,callback){
 }
 
 UserSchema.methods.authenticate = function(password) {
-  return this.password === password;
+  return this.password === this.hashPassword(password);;
 };
 
 UserSchema.post('save', function(next) {
-     if(this.isNew) {
-       console.log('A new user was created.');
-     } else {
-       console.log('A user updated is details.');
-     }
+  if(this.isNew) {
+    console.log('A new user was created.');
+  } else {
+    console.log('A user updated is details.');
+  }
 });
 
-UserSchema.set('toJSON', { getters: true });
+UserSchema.pre('save', function(next) {
+  if (this.password) {
+    this.salt = new Buffer(crypto.randomBytes(16).toString('base64'), 'base64');
+    this.password = this.hashPassword(this.password);
+  }
+  next();
+});
 
+UserSchema.methods.hashPassword = function(password) {
+  return crypto.pbkdf2Sync(password, this.salt, 10000,64).toString('base64');
+};
+
+
+
+// Find a username not already taken
+UserSchema.statics.findUniqueUsername = function(username, suffix, callback) {
+  var _this = this;
+  var possibleUsername = username + (suffix || '');
+  _this.findOne({
+    username: possibleUsername
+  }, function(err, user) {
+    if (!err) {
+      if (!user) {
+        callback(possibleUsername);
+      } else {
+        return _this.findUniqueUsername(username, (suffix || 0) + 1, callback);
+      }
+    } else {
+      callback(null);
+    }});
+};
+
+UserSchema.set('toJSON', { getters: true, virtuals: true });
 mongoose.model('User', UserSchema);
